557b5cc8a2720a5ce343f0b735c1d9f1a82e9a7f
akramguroo-dev <akramguroo.dev@gmail.com>
2026-08-21 17:15:42 +0530

    feat: add application page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import MainLayout from './layouts/MainLayout'
+
 import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Careers from './pages/Careers'
+import FAQ from './pages/FAQ'
+import Contact from './pages/Contact'
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
